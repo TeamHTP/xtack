02e8fb364972aa57b28396227ed43b062d2eed07
Tianyi Ma <tn.ma@outlook.com>
2020-02-09 01:30:07 -0600

    signup ~50%

---
+++ common.js
@@ -0,0 +1,30 @@
+var API_ROOT = 'https://api.xtack.space';
+
+function getApi(endpoint, params, callback) {
+	var xhr = new XMLHttpRequest();
+	xhr.open('GET', `${API_ROOT}${endpoint}${params}`, true);
+	xhr.withCredentials = true;
+	xhr.onreadystatechange = function() {
+	    if (xhr.readyState == 4) {
+	        callback(xhr.responseText, xhr.status);
+	    }
+	}
+	xhr.send(null);
+}
+
+function postApi(endpoint, params, callback) {
+	var xhr = new XMLHttpRequest();
+	xhr.open('POST', `${API_ROOT}${endpoint}`, true);
+	xhr.withCredentials = true;
+	xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+	xhr.onreadystatechange = function() {
+	    if (xhr.readyState == 4) {
+	        callback(xhr.responseText, xhr.status);
+	    }
+	}
+	xhr.send(params);
+}
+
+function isEmailValid(mail) {
+	return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail);
+}
---
+++ signup.js
@@ -0,0 +1,58 @@
+
+var signupApp = new Vue({
+	el: '#signup-form',
+	data: {
+		username: '',
+		email: '',
+		password: '',
+		confirmPassword: '',
+		tos: false,
+		loading: false,
+		usernameError: false,
+		emailError: false,
+		passwordError: false,
+		tosError: false
+	},
+	methods: {
+		signup: function() {
+			if (signupApp.loading) {
+				return;
+			}
+			var error = false;
+			signupApp.usernameError = false;
+			signupApp.emailError = false;
+			signupApp.passwordError = false;
+			signupApp.tosError = false;
+			if (signupApp.username.length < 3 || signupApp.username.length > 24) {
+				signupApp.usernameError = 'Your username must be between 3 and 24 characters.';
+				error = true;
+			}
+			if (!isEmailValid(signupApp.email)) {
+				signupApp.emailError = 'Your email seems to be invalid.';
+				error = true;
+			}
+			if (signupApp.password != signupApp.confirmPassword) {
+				signupApp.passwordError = 'Your passwords don\'t match.';
+				error = true;
+			}
+			if (signupApp.password.length < 6) {
+				signupApp.passwordError = 'Your password must be at least 6 characters long.';
+				error = true;
+			}
+			if (!signupApp.tos) {
+				signupApp.tosError = 'Please accept the terms & conditions.';
+				error = true;
+			}
+			if (!error){
+				signupApp.loading = true;
+				postApi('/account', `username=${signupApp.username}&email=${signupApp.email}&password=${signupApp.password}&tos=${signupApp.tos}`, function(data, status) {
+					if (status === 200) {
+
+					}
+					console.log(data);
+					signupApp.loading = false;
+				});
+			}			
+		}
+	}
+});
