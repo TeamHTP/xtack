edece9a8a922b130904e30f120470f645b9916da
Tianyi Ma <tn.ma@outlook.com>
2020-02-09 05:55:22 -0600

    username

--- menu.js
+++ menu.js
@@ -1,11 +1,17 @@
 var menuApp = new Vue({
 	el: '#menu-app',
 	data: {
-		balance: 0,
+		balance: '...',
 		username: 0,
 	},
 	created: function() {
 		getApi('/wallet', '', function (data, status) {
+			if (status == 403) {
+				location.href = '/signin.html';
+			}
+			menuApp.balance = JSON.parse(data).balance;
+		});
+		getApi('/account', '', function (data, status) {
 			if (status == 403) {
 				location.href = '/signin.html';
 			}
