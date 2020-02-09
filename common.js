e2f431dceae2e8fb6a622894d61f86944daa9264
Tianyi Ma <tn.ma@outlook.com>
2020-02-09 05:30:54 -0600

    Update common.js

--- common.js
+++ common.js
@@ -4,7 +4,10 @@ function getApi(endpoint, params, callback) {
 	var xhr = new XMLHttpRequest();
 	xhr.open('GET', `${API_ROOT}${endpoint}${params}`, true);
 	xhr.withCredentials = true;
-	xhr.setRequestHeader('Authorization', 'Bearer ' + Cookies.get('session_token'));
+	var token = Cookies.get('session_token');
+	if (typeof token !=== 'undefined') {
+		xhr.setRequestHeader('Authorization', `Bearer ${token}`);
+	}
 	xhr.onreadystatechange = function() {
 	    if (xhr.readyState == 4) {
 	        callback(xhr.responseText, xhr.status);
@@ -17,7 +20,10 @@ function postApi(endpoint, params, callback) {
 	var xhr = new XMLHttpRequest();
 	xhr.open('POST', `${API_ROOT}${endpoint}`, true);
 	xhr.withCredentials = true;
-	xhr.setRequestHeader('Authorization', 'Bearer ' + Cookies.get('session_token'));
+	var token = Cookies.get('session_token');
+	if (typeof token !=== 'undefined') {
+		xhr.setRequestHeader('Authorization', `Bearer ${token}`);
+	}
 	xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
 	xhr.onreadystatechange = function() {
 	    if (xhr.readyState == 4) {
