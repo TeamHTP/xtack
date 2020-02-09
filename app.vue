dac98bdfa7761a66883ffb25312bdf67199688aa
marshingjay <jam605@gmail.com>
2020-02-08 19:57:05 -0600

    question list page

---
+++ app.vue
@@ -0,0 +1,26 @@
+
+<template>
+    <div id = "app">
+        {{ message }}
+    </div>
+</template>
+
+
+<script>
+    var app = new Vue({
+        el: '#app',
+        data: {
+            message: 'Hello Vue!',
+            username: 'Jesus Christ'
+        },
+        methods: {
+            getUsername: function() {
+
+            }
+        }
+    })
+</script>
+
+<style scoped>
+    
+</style>
