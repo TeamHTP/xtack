908c53dfdd75e65b33d3bd4aa8f5fe8888187b85
Tianyi Ma <tn.ma@outlook.com>
2020-02-11 18:58:05 -0600

    withdraw logic

--- settings.js
+++ settings.js
@@ -4,7 +4,11 @@ var settings = new Vue({
 		email: '',
 		username: '',
 		xaddress: '',
-		raddress: ''
+		raddress: '',
+		withdrawAddress: '',
+		withdrawAddressError: false,
+		withdrawConfirm: false,
+		withdrawConfirmError: false
 	},
 	created: function() {
 		getApi(`/account`, '', function (data, status) {
@@ -16,5 +20,18 @@ var settings = new Vue({
 			settings.xaddress = JSON.parse(data).addresses.xaddress;
 			settings.raddress = JSON.parse(data).addresses.raddress;
 		});
+	},
+	methods: {
+		withdraw: function() {
+			this.withdrawAddressError = false;
+			this.withdrawConfirmError = false;
+			if (!this.withdrawConfirm) {
+				this.withdrawConfirmError = 'Please recognize that this action is irreversable.';
+				return;
+			}
+			getApi(`/withdraw`, `?address=${withdrawAddress}`, function (data, status) {
+				location.reload();
+			});
+		}
 	}
 })
